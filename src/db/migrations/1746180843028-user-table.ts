8dd3fcb2f6e271a97215249a94243d0b739ff07e
ValberJunior <valberjr1992@gmail.com>
2025-05-02 08:00:08 -0300

    feat(migration): add UUID extension creation in user table migration

--- src/db/migrations/1746180843028-user-table.ts
+++ src/db/migrations/1746180843028-user-table.ts
@@ -2,6 +2,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UserTable1746180843028 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
     await queryRunner.query(`
       CREATE TABLE "user" (
          id uuid NOT NULL DEFAULT uuid_generate_v4(),
