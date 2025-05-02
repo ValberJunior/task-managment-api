7669dbfd4db33c5b8ac7649e3b1093c516d0432b
ValberJunior <valberjr1992@gmail.com>
2025-05-02 08:11:08 -0300

    feat(users): integrate TypeORM with UserEntity in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/db/entities/user.entity';
 
 @Module({
   controllers: [UsersController],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   exports: [UsersService],
   providers: [UsersService],
 })
